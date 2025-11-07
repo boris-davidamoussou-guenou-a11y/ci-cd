206c08d7a8f7868358dff0c0bd7c677a120535c3
Boris-David AMOUSSOU-GUENOU <boris-david.amoussou-guenou@instant-system.com>
2025-11-07 14:00:13 +0100

    Initial commit de mon projet Xcode

---
+++ GeminiApp/GeminiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAppApp.swift
+//  GeminiApp
+//
+//  Created by Instant System iOS Team on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeminiAppTests/GeminiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAppTests.swift
+//  GeminiAppTests
+//
+//  Created by Instant System iOS Team on 07/11/2025.
+//
+
+import Testing
+@testable import GeminiApp
+
+struct GeminiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
